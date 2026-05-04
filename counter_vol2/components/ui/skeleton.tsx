6cea27ebe541844404ea2ed0ca296785f8ae6016
elifsuedaakin <elifsuedaakin@gmail.com>
2026-05-04 14:06:02 +0300

    Fix hydration flicker with isHydrated + skeleton UI with completed files

--- counter_vol2/components/ui/skeleton.tsx
+++ counter_vol2/components/ui/skeleton.tsx
@@ -4,8 +4,8 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
+      className={cn("animate-pulse rounded-md bg-muted", className)} //titreme efekti 
+      {...props} //dışardan gelen her şeyi bu div e geçiriyoruz 
     />
   )
 }
